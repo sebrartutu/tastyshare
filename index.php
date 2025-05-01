3bbec133abab362866aa4f519f0b70fa309df36f
nazlimaynaci <nazlimaync@gmail.com>
2025-05-01 20:37:58 +0200

    index updated.

--- index.php
+++ index.php
@@ -1,32 +1,21 @@
 <?php
 session_start();
-include 'config.php';
+require_once 'config.php';
 
 $page = $_GET['page'] ?? 'main';
-$page_path = "pages/$page.php";
 
-// Header
+if (!array_key_exists($page, $menu)) {
+    $page = 'main'; // varsayılan sayfa
+}
+
 include 'templates/header.php';
 
-// Content
+$page_path = "pages/$page.php";
 if (file_exists($page_path)) {
     include $page_path;
 } else {
     echo "<h2>Page not found.</h2>";
 }
 
-// Footer
 include 'templates/footer.php';
-
-require_once 'config.php';
-
-$page = $_GET['page'] ?? 'home';
-
-if (!array_key_exists($page, $menu)) {
-    $page = 'home'; // Varsayılan sayfa
-}
-
-include 'header.php';
-include 'pages/' . $page . '.php';
-include 'footer.php';
 ?>
