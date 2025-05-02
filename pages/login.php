2f34f6ee2150d585d3b14bb24e7dba5ea14dd1df
Sueda Ebrar TUTU <154499039+sebrartutu@users.noreply.github.com>
2025-05-02 16:53:08 +0200

    update

--- pages/login.php
+++ pages/login.php
@@ -18,6 +18,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
         if ($user && password_verify($password, $user['password'])) {
             $_SESSION['user'] = $user; 
+            [
+              'username' => $user['username'],
+              'name' => $user['family_name'] . ' ' . $user['surname']
+            ];
             header("Location: index.php"); 
             exit;
         } else {
