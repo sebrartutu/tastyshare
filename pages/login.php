8edb983749afa1491ca2688672c2b6a0dd704d51
Sueda Ebrar TUTU <154499039+sebrartutu@users.noreply.github.com>
2025-05-02 08:06:45 +0200

    update

--- pages/login.php
+++ pages/login.php
@@ -39,4 +39,4 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <button type="submit">Login</button>
 </form>
 
-<p>Don't have an account? <a href="register.php">Register here</a></p>
+<p>Don't have an account? <a href="index.php?page=register">Register here</a></p>
