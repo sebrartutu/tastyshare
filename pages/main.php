2a25d92534224bd71c9f9659a231c9453d13b72d
nazlimaynaci <nazlimaync@gmail.com>
2025-05-02 22:18:06 +0200

    video and map added.

--- pages/main.php
+++ pages/main.php
@@ -16,3 +16,8 @@
     src="https://www.google.com/maps/embed?pb=!1m18!..." 
     width="600" height="450" style="border:0;" allowfullscreen loading="lazy">
 </iframe>
+<h3>Recipe Video</h>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/lJ7Ec3bKrxM?si=hY6T51roY8KuJcHR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    
+    <h3>Where We Cook</h3>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2726.3377163909568!2d19.666564676941153!3d46.8960762711334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743da7a6c479e1d%3A0xc8292b3f6dc69e7f!2sJohn%20von%20Neumann%20University%20GAMF%20Faculty%20of%20Engineering%20and%20Computer%20Science!5e0!3m2!1str!2shu!4v1746216928200!5m2!1str!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
